65cb5dad9d6a692eda85d16f34371bf6ed835366
Hannah Carl <hcarl@boomtownroi.com>
2020-06-30 16:31:16 -0400

    Add classes, update readme, cleanup

--- src/apiDataVerification.php
+++ src/apiDataVerification.php
@@ -2,49 +2,53 @@
 // Completed by Hannah Carl
 include 'apiDataPull.php';
 
-// Function verifies the updated date is later than created from the API
-function verifyDate($createdAt, $updatedAt){
-  $createdTime = new DateTime($createdAt);
-  $updatedTime = new DateTime($updatedAt);
+class apiDataVerify {
 
-  printf("-----------------------------------------------------------------------\n");
-  printf("\nDate Verification\n");
+  // Function verifies the updated date is later than created from the API
+  public function verifyDate($createdAt, $updatedAt): void {
+    $createdTime = new DateTime($createdAt);
+    $updatedTime = new DateTime($updatedAt);
 
-  if($updatedTime > $createdTime){
-    echo "Updated time of " . $updatedAt . " is later than the created time of " . $createdAt . "\n";
+    printf("-----------------------------------------------------------------------\n");
+    printf("\nDate Verification\n");
+
+    if($updatedTime > $createdTime){
+      echo "Updated time of " . $updatedAt . " is later than the created time of " . $createdAt . "\n";
+    }
+    else{
+      echo "Failure: Updated time was not later than the created time\n";
+    }
   }
-  else{
-    echo "Failure: Updated time was not later than the created time\n";
+
+  // Function verifies repository count
+  public function verifyReposCount($publicReposCount, $reposURL, $context): void {
+    $pageNumber = 1;
+    $urlRepoCount = 0;
+
+    // Loop until counter is greater than or matches expected count
+    // Secoundary check on page number to ensure that it doesn't continue to search if the page is 
+    // a 404 or doesn't have values. It's set to 10, but this could be changed in the future if needed.
+    while(($urlRepoCount < $publicReposCount) && ($pageNumber < 10)) {
+      $repoURLInfo = file_get_contents($reposURL . "?page=" . $pageNumber, false, $context);
+      $urlRepoCount += substr_count($repoURLInfo, "full_name");
+      $pageNumber += 1;
+    }
+
+    printf("-----------------------------------------------------------------------\n");
+    printf("\nRepository Count Verification\n");
+
+    if($urlRepoCount === $publicReposCount){
+      echo "Repository Counter: Verified\n";
+      echo "Repository Count: " . $urlRepoCount . "\n";
+    }
+    else{
+      echo "Respository Counter: Not Verified\n";
+    }
   }
 };
 
-// Function verifies repository count
-function verifyReposCount($publicReposCount, $reposURL, $context){
-  $pageNumber = 1;
-  $urlRepoCount = 0;
-
-  // Loop until counter is greater than or matches expected count
-  // Secoundary check on page number to ensure that it doesn't continue to search if the page is 
-  // a 404 or doesn't have values. It's set to 10, but this could be changed in the future if needed.
-  while(($urlRepoCount < $publicReposCount) && ($pageNumber < 10)) {
-    $repoURLInfo = file_get_contents($reposURL . "?page=" . $pageNumber, false, $context);
-    $urlRepoCount += substr_count($repoURLInfo, "full_name");
-    $pageNumber += 1;
-  }
-
-  printf("-----------------------------------------------------------------------\n");
-  printf("\nRepository Count Verification\n");
-
-  if($urlRepoCount === $publicReposCount){
-    echo "Repository Counter: Verified\n";
-    echo "Repository Count: " . $urlRepoCount . "\n";
-  }
-  else{
-    echo "Respository Counter: Not Verified\n";
-  }
-};
-
-// Functions to run
-verifyDate($mainArray["created_at"],$mainArray["updated_at"]);
-verifyReposCount($mainArray["public_repos"],$mainArray["repos_url"], $context);
+// Create new instance and run functions
+$apiDataVerify = new apiDataVerify;
+$apiDataVerify->verifyDate($mainArray["created_at"],$mainArray["updated_at"]);
+$apiDataVerify->verifyReposCount($mainArray["public_repos"],$mainArray["repos_url"], $context);
 ?>
