99bdb65aea2040f86266fb2e106ff9857d6f675b
Hannah Carl <hcarl@boomtownroi.com>
2020-06-30 15:12:24 -0400

    Main code for api data pull and verification

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # BoomTownROITechnicalAssessmentPHP
+---
+
+## To run 
+1. Clone repo
+2. 'cd' your path to the /src folder
+3. Run 'php apiDataVerification.php'
+
+---
+
+## Output
+**1. Lists URL's status codes and relevant ID's with object name if applicable**
+-----------------------------------------------------------------------
+|   URL |https://api.github.com/orgs/BoomTownROI/member               |
+|Status |HTTP/1.1 404 Not Found                                       |
+-----------------------------------------------------------------------
+|   URL |https://api.github.com/orgs/BoomTownROI/public_members       |
+|Status |HTTP/1.1 200 OK                                              |
+-----------------------------------------------------------------------
+|   Key |Value                                                        |
+|    id |12473631                                                     |
+|    id |5915162                                                      |
+|    id |646406                                                       |
+|    id |682368                                                       |
+|    id |5933413                                                      |
+|    id |171873                                                       |
+|    id |3579671                                                      |
+|    id |228283                                                       |
+|    id |12699906                                                     |
+|    id |6516782                                                      |
+|    id |1153690                                                      |
+|    id |7852616                                                      |
+-----------------------------------------------------------------------
+|   URL |https://api.github.com/orgs/BoomTownROI/member               |
+|Status |HTTP/1.1 404 Not Found                                       |
+
+**2. Date Verification**
+-----------------------------------------------------------------------
+
+Date Verification
+Updated time of 2020-04-21T23:30:09Z is later than the created time of 2011-11-22T21:48:43Z
+
+**3. Repository Count Verification**
+-----------------------------------------------------------------------
+
+Repository Count Verification
+Repository Counter: Verified
+Repository Count: 41
---
+++ src/apiDataPull.php
@@ -0,0 +1,101 @@
+<?php
+// Completed by Hannah Carl
+
+// Global variables
+$githubAPIURL = 'https://api.github.com/orgs/BoomTownROI';
+$urlAPIArray = array();
+$mainArray = array();
+
+// Setup context
+$context = stream_context_create(
+  array(
+      "http" => array(
+          "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+      )
+  )
+);
+
+// Get information from API
+function getGithubAPIInfo($githubAPIURL, $context) {
+
+  // The line below pulls directly from the live github api
+  $mainJSON = file_get_contents($githubAPIURL, false, $context);
+  // To prevent excessive pulls, I used a downloaded json in /resources for developing
+  //$mainFilePath = '../resources/main.json';
+  //$mainJSON = file_get_contents($mainFilePath);
+  $mainArray = json_decode($mainJSON, true);
+
+  return $mainArray;
+
+};
+
+function getGithubAPIURL($githubAPIURL, $mainArray){
+  $urlArray = array();
+
+  // Find url values that include "api.github.com/orgs/BoomTownROI"
+  // Parse through any secondary urls and make sure those are included
+  foreach ($mainArray as $mainValue) {
+    if (strpos($mainValue, $githubAPIURL) !== false) { 
+      // Clean up secondary url location
+      if(strpos($mainValue, '{') !== false) { 
+        $mainValue = str_replace('}', '', $mainValue);
+        $secondaryURL = explode('{',$mainValue);
+        array_push($urlArray, $secondaryURL[0], $githubAPIURL . $secondaryURL[1]);
+      }
+      else{
+        $urlArray[] = $mainValue; 
+      }
+    }
+  }
+  return $urlArray;
+};
+
+// Check the status of a request for each URL
+function checkURLStatus($urlAPIArray, $context, $githubAPIURL) {
+  $mask = "|%6.6s |%-60.60s |\n";
+
+  foreach ($urlAPIArray as $urlToVisit) {
+    $headerArray = get_headers($urlToVisit, 1, $context);
+
+    // Failed status
+    if(strpos($headerArray[0], '200') === false) { 
+      printf("-----------------------------------------------------------------------\n");
+      printf($mask, "URL", $urlToVisit);
+      printf($mask, 'Status', $headerArray[0]);
+    }
+    // 200 response status
+    else {
+      $pageInfo = file_get_contents($urlToVisit, 1, $context);
+      $subJSONObj = json_decode($pageInfo);
+      printf("-----------------------------------------------------------------------\n");
+      printf($mask, "URL", $urlToVisit);
+      printf($mask, 'Status', $headerArray[0]);
+      printf("-----------------------------------------------------------------------\n");
+      printf($mask, 'Key', 'Value');
+      // each sub directory of https://api.github.com/orgs/BoomTownROI
+      if($urlToVisit !== $githubAPIURL){
+        foreach($subJSONObj as $row) {
+          foreach($row as $key => $val) {
+            if($key === "id"){
+              printf($mask, $key, $val);
+            }
+          }
+        }
+      }
+      // main https://api.github.com/orgs/BoomTownROI page
+      else{
+        foreach($subJSONObj as $key => $val) {
+          if($key === "id"){
+            printf($mask, $key, $val);
+          }
+        }
+      }
+    }
+  };
+};
+
+// Functions to run
+$mainArray = getGithubAPIInfo($githubAPIURL, $context);
+$urlAPIArray = getGithubAPIURL($githubAPIURL, $mainArray);
+checkURLStatus($urlAPIArray, $context, $githubAPIURL);
+?>
